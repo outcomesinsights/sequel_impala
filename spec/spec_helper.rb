9487bf44203253dc9b7f482a2671759661a8c8dc
Jeremy Evans <code@jeremyevans.net>
2015-11-07 11:26:53 -0800

    Add starting spec suite
    This is a simple copy of Sequel's dataset integration test, except
    that the guards have been removed from the specs.
    
    Current results:
    
    132 runs, 69 assertions, 30 failures, 89 errors, 0 skips in 465 seconds

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'logger'
+require 'sequel'
+
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/hooks/default'
+require 'minitest/shared_description'
+
+Sequel::Model.cache_associations = false if ENV['SEQUEL_NO_CACHE_ASSOCIATIONS']
+Sequel.cache_anonymous_models = false
+
+unless defined?(DB)
+  DB = Sequel.connect(ENV['IMPALA_URL'] || 'jdbc:hive2://localhost:21050/;auth=noSasl')
+end
