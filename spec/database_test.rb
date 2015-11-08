fcbc45d13fca98f9754fc94f1af2534cf8882ad7
Jeremy Evans <code@jeremyevans.net>
2015-11-08 09:43:24 -0800

    Remove constraint violation specs, since Impala doesn't support constraints
    Final Results:
    7 runs, 8 assertions, 0 failures, 0 errors, 0 skips in 6 seconds

--- spec/database_test.rb
+++ spec/database_test.rb
@@ -24,63 +24,6 @@ describe Sequel::Database do
     proc{@db << "S"}.must_raise(Sequel::DatabaseError)
   end
 
-  it "should have Sequel::DatabaseError#sql give the SQL causing the error" do
-    (@db << "SELECT") rescue (e = $!)
-    e.sql.must_equal "SELECT"
-  end if ENV['SEQUEL_ERROR_SQL']
-
-  describe "constraint violations" do
-    before do
-      @db.drop_table?(:test2, :test)
-    end
-    after do
-      @db.drop_table?(:test2, :test)
-    end
-
-    it "should raise Sequel::UniqueConstraintViolation when a unique constraint is violated" do
-      @db.create_table!(:test){String :a, :unique=>true, :null=>false}
-      @db[:test].insert('1')
-      proc{@db[:test].insert('1')}.must_raise(Sequel::UniqueConstraintViolation)
-      @db[:test].insert('2')
-      proc{@db[:test].update(:a=>'1')}.must_raise(Sequel::UniqueConstraintViolation)
-    end
-
-    it "should raise Sequel::UniqueConstraintViolation when a unique constraint is violated for composite primary keys" do
-      @db.create_table!(:test){String :a; String :b; primary_key [:a, :b]}
-      @db[:test].insert(:a=>'1', :b=>'2')
-      proc{@db[:test].insert(:a=>'1', :b=>'2')}.must_raise(Sequel::UniqueConstraintViolation)
-      @db[:test].insert(:a=>'3', :b=>'4')
-      proc{@db[:test].update(:a=>'1', :b=>'2')}.must_raise(Sequel::UniqueConstraintViolation)
-    end
-
-    it "should raise Sequel::CheckConstraintViolation when a check constraint is violated" do
-      @db.create_table!(:test){String :a; check Sequel.~(:a=>'1')}
-      proc{@db[:test].insert('1')}.must_raise(Sequel::CheckConstraintViolation)
-      @db[:test].insert('2')
-      proc{@db[:test].insert('1')}.must_raise(Sequel::CheckConstraintViolation)
-    end
-
-    it "should raise Sequel::ForeignKeyConstraintViolation when a foreign key constraint is violated"  do
-      @db.create_table!(:test, :engine=>:InnoDB){primary_key :id}
-      @db.create_table!(:test2, :engine=>:InnoDB){foreign_key :tid, :test}
-      proc{@db[:test2].insert(:tid=>1)}.must_raise(Sequel::ForeignKeyConstraintViolation)
-      @db[:test].insert
-      @db[:test2].insert(:tid=>1)
-      proc{@db[:test2].where(:tid=>1).update(:tid=>3)}.must_raise(Sequel::ForeignKeyConstraintViolation)
-      proc{@db[:test].where(:id=>1).delete}.must_raise(Sequel::ForeignKeyConstraintViolation)
-    end
-
-    it "should raise Sequel::NotNullConstraintViolation when a not null constraint is violated" do
-      @db.create_table!(:test){Integer :a, :null=>false}
-      proc{@db[:test].insert(:a=>nil)}.must_raise(Sequel::NotNullConstraintViolation)
-      unless @db.database_type == :mysql
-        # Broken mysql silently changes NULL here to 0, and doesn't raise an exception.
-        @db[:test].insert(2)
-        proc{@db[:test].update(:a=>nil)}.must_raise(Sequel::NotNullConstraintViolation)
-      end
-    end
-  end
-
   it "should store underlying wrapped exception in Sequel::DatabaseError" do
     begin
       @db << "SELECT"
