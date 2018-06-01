3722d604ab12cfce1a9bf5a586fa87b449c3e75e
Jeremy Evans <code@jeremyevans.net>
2018-05-31 18:43:36 -0700

    Get tests running again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
-require 'rubygems'
+$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 require 'logger'
 require 'sequel'
-
-module Minitest
-  @@installed_at_exit = true
-end if defined?(M)
 
 gem 'minitest'
 require 'minitest/autorun'
@@ -12,7 +8,7 @@ require 'minitest/hooks/default'
 require 'minitest/shared_description'
 
 Sequel::Model.cache_associations = false if ENV['SEQUEL_NO_CACHE_ASSOCIATIONS']
-Sequel.cache_anonymous_models = false
+Sequel::Model.cache_anonymous_models = false
 
 unless defined?(DB)
   DB = Sequel.connect(ENV['IMPALA_URL'] || 'jdbc:hive2://localhost:21050/;auth=noSasl')
