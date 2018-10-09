0004e48dfc6d205081dff159acdb65aa60e0e439
Ryan Duryea <aguynamedryan@gmail.com>
2018-10-09 10:13:32 -0700

    Travis: try to get tests running again

--- .travis.sh
+++ .travis.sh
@@ -2,14 +2,14 @@
 
 set -x
 
+env | sort
+
 eval "$(ssh-agent -s)"
 ssh-add <(echo "${IMPALA_CLUSTER_PRIVATE_KEY_BASE64}" | base64 --decode)
 ssh-add -l
 
 ssh -o "StrictHostKeyChecking=no" ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com ping -c 1 nonworker1.hadoop.jsaw.io || exit 0
 ssh -o "StrictHostKeyChecking=no" -M -S my-ctrl-socket -fnNT -L 21000:nonworker1.hadoop.jsaw.io:21000 ec2-user@ec2-52-40-86-190.us-west-2.compute.amazonaws.com
-
-env | sort
 
 time bundle exec rake TESTOPTS="--profile --fail-fast"
 result=$?
